a27d287064c28df67bda11f890fab25f5a7a4724
Sergeman <52895317+Sergecodes@users.noreply.github.com>
2021-07-25 01:24:16 +0100

    Update _document.js
    Applied getInitiaProps to preload server stylesheets

--- pages/_document.js
+++ pages/_document.js
@@ -19,20 +19,20 @@ export default class MyDocument extends Document {
   }
 }
 
-// MyDocument.getInitialProps = async (ctx) => {
-//     const sheets = new ServerStyleSheets();
-//     const originalRenderPage = ctx.renderPage;
-//
-//     ctx.renderPage = () =>
-//       originalRenderPage({
-//         enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-//       });
-//
-//     const initialProps = await Document.getInitialProps(ctx);
-//
-//     return {
-//       ...initialProps,
-//       // Styles fragment is rendered after the app and page rendering finish.
-//       styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-//     };
-// };
+MyDocument.getInitialProps = async (ctx) => {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    };
+};
